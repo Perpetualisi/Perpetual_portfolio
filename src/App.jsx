4ee9ceda6665a3bc31b503b89a74775380120684
Isi Name <perpetualokan5@gmail.com>
2025-05-19 12:27:34 +0100

    Fix mobile white space issue and enhance layout

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,20 @@ import About from './Components/About/About';
 import Projects from './Components/Projects/Projects';
 import Skills from './Components/Skills/Skills';
 import Contact from './Components/Contact/Contact';
+
 const App = () => {
   return (
     <Router>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Hero />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path='/skills' element={<Skills />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+      <div className="main-container">
+        <Routes>
+          <Route path="/" element={<Hero />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
     </Router>
   );
 };
